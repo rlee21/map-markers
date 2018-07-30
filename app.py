e8473cd0d16e086022a68bd95b34ef0115700aaf
rlee21 <rlee21@comcast.net>
2018-07-29 23:41:41 -0700

    update app.py

--- app.py
+++ app.py
@@ -21,6 +21,4 @@ def index():
 
 
 if __name__ == '__main__':
-    host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host=host, port=port)
+    app.run()
