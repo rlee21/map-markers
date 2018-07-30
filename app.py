31af999c6a262a6786792f06c76f0e89dca3f3c0
rlee21 <rlee21@comcast.net>
2018-07-29 23:19:10 -0700

    add Profile and update app.py

--- app.py
+++ app.py
@@ -21,4 +21,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = os.environ.get('IP', '0.0.0.0')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host=host, port=port)
